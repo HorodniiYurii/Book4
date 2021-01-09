29ad0cf8bcd06c64b71e1032857a13aa05ed5c85
Yura Horodnii <yurahorodnii@gmail.com>
2021-01-09 12:27:24 +0200

    Add project files.

---
+++ Book4/Migrations/202101091024540_1.cs
@@ -0,0 +1,57 @@
+﻿namespace Book4.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Author",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.BookReview",
+                c => new
+                    {
+                        Id = c.Int(nullable: false),
+                        ReviewerName = c.String(nullable: false),
+                        Rating = c.Int(nullable: false),
+                        BookId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Book", t => t.Id)
+                .Index(t => t.Id);
+            
+            CreateTable(
+                "dbo.Book",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 512),
+                        AuthorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Author", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BookReview", "Id", "dbo.Book");
+            DropForeignKey("dbo.Book", "AuthorId", "dbo.Author");
+            DropIndex("dbo.Book", new[] { "AuthorId" });
+            DropIndex("dbo.BookReview", new[] { "Id" });
+            DropTable("dbo.Book");
+            DropTable("dbo.BookReview");
+            DropTable("dbo.Author");
+        }
+    }
+}
